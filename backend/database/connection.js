b1f4a822fd3c15f6fa9c06677576ac8abfd9e528
NIKHIL KUMAR <nikhilkumarhp2003@gmail.com>
2025-04-08 10:59:52 +0530

    Initial commit

---
+++ backend/database/connection.js
@@ -0,0 +1,12 @@
+ import mongoose from"mongoose";
+ export const connection=()=>{  
+    
+    mongoose.connect(process.env.MONGO_URI,{
+       dbName:"MERN_AUCTION_PLATFORM",
+   }).then(()=>{   
+       console.log("connected to database.");
+    })  .catch(err=>{
+       console.log(`some error ocuured while connecting:${err}`);
+          }) ;
+    };
+  
